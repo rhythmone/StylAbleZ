4b5145fc30a4abeebd58b820a77f185b940c44af
Joshua DeFord <joshdeford@onprem.com>
2021-05-25 20:06:07 -0500

    Finishing the filename parser and tests.

--- src/stylablez/functions/filenameParser.test.ts
+++ src/stylablez/functions/filenameParser.test.ts
@@ -1,4 +1,4 @@
-import parseStylablezFilename from "./filenameParser";
+import parseStylablezFilename, {parseStylablezFiles} from "./filenameParser";
 
 const z = 't01_n-shad1_br105_ma1_clS'
 const a = '02_n-shad2_op40_ma1_clS_blML'
@@ -7,7 +7,62 @@ const c = '04_n-eyeLiner_op50_ma1_clA'
 const d = '05_n-lash_ma1_clA'
 const e = '06_n-eye'
 
-test('Token splitting gices tonkens', () => {
+test('A filename with one of each of the tokens is correctly parsed', () => {
+  const fileName = `010_eyes-brown_UR12_VIF_CLA_BLDC`
+  const styles = parseStylablezFilename(fileName)
+  expect(styles).toMatchSnapshot()
+})
+
+test('A filename with one of each of the tokens is correctly parsed with no regard to casae', () => {
+  const fileName = `010_EYEs-bROwn_uR12_ViF_cLa_BlDc`
+  const styles = parseStylablezFilename(fileName)
+  expect(styles).toMatchSnapshot()
+})
+
+test('Many filenames are correctly ordered.', () => {
+  const styles = parseStylablezFiles([
+    `099_eyes-brown_UR99_VIF_CLA_BLDC`,
+    `010_-brown_UR10_VIF_CLA_BLDC`,
+    `012_-brown_UR12_VIF_CLA_BLDC`,
+    `002_-brown_UR2_VIF_CLA_BLDC`,
+    `030_-brown_UR30_VIF_CLA_BLDC`,
+    `100_-brown_UR100_VIF_CLA_BLDC`,
+    `909__eyes-brown_UR909_VIF_CLA_BLDC`,
+   ])
+  expect(styles).toMatchSnapshot()
+})
+
+test('A filename with an incorrect blend mode throws an error', () => {
+  const fileName = `099_eyes-brown_UR12_VIF_CLA_BLXX`
+  expect(() => {
+    const styles = parseStylablezFilename(fileName)
+  }).toThrowErrorMatchingSnapshot();
+})
+
+test('A filename with an unrecognized token throws an error', () => {
+  const fileName = `099_eyes-brown_UR12_VIF_CLA_XLDC`
+  expect(() => {
+    const styles = parseStylablezFilename(fileName)
+  }).toThrowErrorMatchingSnapshot();
+})
+
+test('A filename with an incorrect boolean value throws an error', () => {
+  const fileName = `099_eyes-brown_UR12_VIY_CLA_BLDC`
+  expect(() => {
+    const styles = parseStylablezFilename(fileName)
+  }).toThrowErrorMatchingSnapshot();
+})
+
+test('A filename with an incorrect numeric value throws an error', () => {
+  const fileName = `099_eyes-brown_UR1s_VIF_CLA_BLDC`
+  expect(() => {
+    const styles = parseStylablezFilename(fileName)
+  }).toThrowErrorMatchingSnapshot();
+})
+
+
+/*
+test('A bunch of files are parsed', () => {
     expect(1 + 2).toBe(3);
     const testNames = [z, a, b, c, d, e]
     testNames.forEach((testName) => {
@@ -15,3 +70,4 @@ test('Token splitting gices tonkens', () => {
     })
     console.log(testNames.join('\n').toUpperCase())
 });
+ */
--- src/stylablez/functions/filenameParser.ts
+++ src/stylablez/functions/filenameParser.ts
@@ -23,6 +23,12 @@ const booleanTokenMap: {[key: string]: string} = {
     'MA': 'mask',
 }
 
+const colorVariantMap: {[key: string]: string} = {
+    'A': 'accent',
+    'P': 'primary',
+    'S': 'secondary',
+}
+
 const colorTokenMap: {[key: string]: string} = {
     'CL': 'backgroundColor',
 }
@@ -45,58 +51,118 @@ export const blendModeMap: {[key: string]: string} = {
     'HU': 'hue'
 }
 
-const digitsOnly = /^[0-9]*$/.compile()
-const firstCharacters = /(^[A-Z|a-z]{2})(.*)/.compile()
+const digitsOnly = /(^[0-9]*$)/
+const styleCode = /(^[A-Z|a-z]{2})(.*)/
 
-const parseStylablezFilename = (fileName: string) => {
+interface Stylable {
+    layerOrder: number,
+    styleMap: StylableZ
+}
+
+interface StylableZ {[styleName: string]: string | number | boolean}
+
+
+export const parseStylablezFiles = (filenames: string[]): StylableZ[] => {
+    const stylables = filenames.map((name) => parseStylablezFilename(name))
+    stylables.sort((s1, s2) => {
+        if (s1.layerOrder > s2.layerOrder) {
+            return 1
+        } else if (s1.layerOrder < s2.layerOrder) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+    return stylables.map(s => s.styleMap);
+}
+
+const parseStylablezFilename = (fileName: string): Stylable => {
+    let layerOrder
     const stripped = fileName.replace(/\s+/g, '')
     const tokens = stripped.toUpperCase().split(tokenDelimiter)
     const styleMap: {[key: string]: number | boolean | string} = {}
-    let tokenConfigIndex = 0
     const cleanTokens = tokens.filter(token => token.indexOf('-') < 0)
     cleanTokens.forEach(token => {
-        const found = token.match(digitsOnly)
-        if (found) {
-            const tokenKey = found[1] as string
-            const tokenValue = found[1]
+        const digitsFound = token.match(digitsOnly)
+        const styleCodeFound = token.match(styleCode)
+        if (digitsFound) {
+            layerOrder = Number.parseInt(digitsFound[1], 10)
+        } else if (styleCodeFound) {
+            const tokenKey = styleCodeFound[1] as string
+            const tokenValue = styleCodeFound[2]
             if (effectTokenMap[tokenKey] !== undefined) {
-                const parsedValue = Number.parseInt(tokenValue, 10)
-                if (Number.isNaN(parsedValue)) {
-                    throw Error(`The part of the filename: '${fileName}' containing the code: '${token}'
-                    is followed by a value: '${tokenValue}' that can't be converted to a number.
-                    It must be a number`)
-                }
-                styleMap[effectTokenMap[tokenKey]] = parsedValue
+                addEffectValue(styleMap, fileName, token, tokenKey, tokenValue);
             } else if (blendModeTokenMap[tokenKey] !== undefined) {
-                if (blendModeMap[tokenValue] !== undefined) {
-                    styleMap[effectTokenMap[tokenKey]] = blendModeMap[tokenValue]
-                } else {
-                    throw Error(`The part of the filename: '${fileName}' containing the blend mode code: '${token}'
-                    is followed by a value that isn't a code for a blend mode. 
-                    The allowed codes for the blend modes are: 
-                    ${JSON.stringify(blendModeMap, null, 2)}`)
-                }
+                addBlendMode(styleMap, fileName, token, tokenKey, tokenValue);
             } else if (booleanTokenMap[tokenKey] !== undefined) {
-                if (tokenValue !== 'T' && tokenValue !== 'F') {
-                    throw Error(`The part of the filename: '${fileName}' containing the code: '${token}'
-                    is followed by a code that can't be converted to true of false.  It's ${tokenValue}. 
-                    It must be either 'T' or 'F'`)
-                }
-                styleMap[tokenKey] = tokenValue === 'T'
+                addBooleanValue(styleMap, fileName, token, tokenKey, tokenValue);
             } else if (colorTokenMap[tokenKey] !== undefined) {
+                addColorVariant(styleMap, fileName, token, tokenKey, tokenValue);
             } else {
                 throw Error(`The part of the filename: '${fileName}' containing ${token} isn't a valid code in the filename. '. 
-                It should be one of these: ${tokens}.  Consult the documentation to
+                It should be one of these: ${cleanTokens.join(", ")}.  Consult the documentation to
                 determine which combinations of letters and numbers are valid in the filename.`)
             }
-
-            tokenConfigIndex = parseInt(token, 10)
-        } else if (token.match(firstCharacters)) {
-            throw Error(`The part of the filename: '${fileName}' containing ${token} didn't look like anything that
-            matched the default style names. The are: ${tokens}.  Consult the documentation to
-            determine which combinations of letters and numbers are valid in the filename.`)
+        } else {
+            let message = `The part of the filename: '${fileName}' containing ${token} didn't look like anything that matched codes for the default style names.\n\n`
+            message += `Valid codes with numeric values following them are: ${JSON.stringify(effectTokenMap, null, 2)}\n\n`
+            message += `Valid codes with T or F following them are: ${JSON.stringify(booleanTokenMap, null, 2)}\n\n`
+            message += `Valid codes with palette color (A, P or S) following them are: ${JSON.stringify(colorTokenMap, null, 2)}\n\n`
+            message += `Valid codes with a blend following them are: ${JSON.stringify(blendModeTokenMap, null, 2)}\n\n`
+            message += `Valid blend modes are: ${JSON.stringify(blendModeMap, null, 2)}\n\n`
+            message += `Consult the documentation to determine which combinations of letters and numbers are valid in the filename.`
+            throw Error(message)
         }
     })
+
+    if (layerOrder === undefined) {
+        throw Error(`The filename: '${fileName}' must begin with a number which indicates what order it should be inserted into the stack.`)
+    }
+
+    return {
+        layerOrder,
+        styleMap
+    }
+}
+
+
+export const addEffectValue = (styleMap: StylableZ, fileName: string, token: string, tokenKey: string, tokenValue: string) => {
+    if (isNaN(tokenValue as any)) {
+        throw Error(`The part of the filename: '${fileName}' containing the code: '${token}'
+                    is followed by a value: '${tokenValue}' that can't be converted to a number.
+                    It must be a number`)
+    }
+    styleMap[effectTokenMap[tokenKey]] =  Number.parseInt(tokenValue, 10)
+}
+
+export const addBlendMode = (styleMap: StylableZ, fileName: string, token: string, tokenKey: string, tokenValue: string) => {
+    if (blendModeMap[tokenValue] !== undefined) {
+        styleMap[blendModeTokenMap[tokenKey]] = blendModeMap[tokenValue]
+    } else {
+        throw Error(`The part of the filename: '${fileName}' containing the blend mode code: '${token}'
+                    is followed by a value that isn't a code for a blend mode. '${tokenValue}' is not a blend mode. 
+                    The allowed codes for the blend modes are: 
+                    ${JSON.stringify(blendModeMap, null, 2)}`)
+    }
+}
+
+export const addBooleanValue = (styleMap: StylableZ, fileName: string, token: string, tokenKey: string, tokenValue: string) => {
+    if (tokenValue !== 'T' && tokenValue !== 'F') {
+        throw Error(`The part of the filename: '${fileName}' containing the code: '${token}'
+                    is followed by a code that can't be converted to true of false.  It's '${tokenValue}'. 
+                    It must be either 'T' or 'F'`)
+    }
+    styleMap[booleanTokenMap[tokenKey]] = tokenValue === 'T'
+}
+
+function addColorVariant(styleMap: StylableZ, fileName: string, token: string, tokenKey: string, tokenValue: string) {
+    if (tokenValue === 'A' || tokenValue === 'P' || tokenValue === 'S') {
+        styleMap[colorTokenMap[tokenKey]] = colorVariantMap[tokenValue]
+    } else {
+        throw Error(`The part of the filename: '${fileName}' containing the code: '${token}'
+                    is followed by a code that can't be converted to primary, secondary or accent.
+                    It must be either 'A', 'P' or 'S'`)
+    }
 }
 
 export default parseStylablezFilename
